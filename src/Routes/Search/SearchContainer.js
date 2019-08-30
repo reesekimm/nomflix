1688ef4f9837d98f0d0efbf28a06fa63f1685b96
Nanhyun Kim <nanhyunkim@gmail.com>
2019-08-30 23:01:37 +0900

    Update Data Request part

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SearchPresenter from './SearchPresenter';
+
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: '',
+    error: null,
+    loading: false // 사용자가 검색하기 전에는 결과를 로딩하지 않음
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: "994809ea1676b31c23f911a030d67191",
+        language: "en-US"
+    }
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) => api.get(`movie/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  search: (term) => api.get("search/movie", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  tvDetail: (id) => api.get(`tv/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  search: (term) => api.get("search/tv", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+};
