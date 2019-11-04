c9b110c43e022d5ffe1ffa09b0b59d2634fe922a
Nanhyun Kim <nanhyunkim@gmail.com>
2019-11-04 16:17:24 +0900

    BrowserRouter -> HashRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom';
-import Header from 'Components/Header';
-import Home from 'Routes/Home';
-import TV from 'Routes/TV';
-import Search from 'Routes/Search';
-import Detail from 'Routes/Detail';
+} from "react-router-dom";
+import Header from "Components/Header";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
